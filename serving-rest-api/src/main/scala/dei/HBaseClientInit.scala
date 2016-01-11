8e8a8e6b3c401e055ededb2334ea806a7e2e956c
warshmellow <ddwchensf@gmail.com>
2016-01-11 12:56:25 -0800

    Change pom.xml and JettyLauncher to allow deployment as WAR on Java 1.7

--- serving-rest-api/src/main/scala/dei/HBaseClientInit.scala
+++ serving-rest-api/src/main/scala/dei/HBaseClientInit.scala
@@ -5,4 +5,11 @@ import org.apache.hadoop.hbase.HBaseConfiguration
 
 trait HBaseClientInit {
   val hBaseConfig: Configuration = HBaseConfiguration.create
+  hBaseConfig.set("hbase.master","127.0.0.1:16000")
+  hBaseConfig.set("hbase.root", "hdfs://127.0.0.1:8020")
+  hBaseConfig.set("hbase.zookeeper.quorum","127.0.0.1")
+  hBaseConfig.set("hbase.zookeeper.property.clientPort","2181")
+  hBaseConfig.set("zookeeper.znode.parent","/hbase-unsecure")
+  hBaseConfig.set("hbase.master.port","16000")
+  hBaseConfig.set("hbase.regionserver.port","16020")
 }
